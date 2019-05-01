94e9d5da663fe8ed0049b7c4920ded73b84c47e4
saltecrez <elisa.londero@inaf.it>
2019-05-01 17:32:19 +0200

    Completed chapter 9

---
+++ src/chapter9/QueueEmptyException.java
@@ -0,0 +1,10 @@
+package chapter9;
+
+// An exception for queue-empty errors. 
+
+public class QueueEmptyException extends Exception {
+
+    public String toString() {
+        return "\nQueue is empty.";
+    }
+}
