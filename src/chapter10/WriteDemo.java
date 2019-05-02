22586b5fa18b92619292d633d29b4947f56de8ca
saltecrez <elisa.londero@inaf.it>
2019-05-02 10:37:47 +0200

    Started chapter 10

---
+++ src/chapter10/WriteDemo.java
@@ -0,0 +1,13 @@
+package chapter10;
+
+// Demonstrate System.out.write(). 
+class WriteDemo {
+
+    public static void main(String args[]) {
+        int b;
+
+        b = 'Y';
+        System.out.write(b);
+        System.out.write('\n');
+    }
+}
