3986d02b38805cd1b7c10ff929259f004a19b5d7
saltecrez <elisa.londero@inaf.it>
2019-05-19 20:56:38 +0200

    Autoboxing

---
+++ src/chapter12/Wrap.java
@@ -0,0 +1,14 @@
+package chapter12;
+
+// Demonstrate manual boxing and unboxing with a type wrapper. 
+class Wrap {
+
+    public static void main(String args[]) {
+
+        Integer iOb = new Integer(100);
+
+        int i = iOb.intValue();
+
+        System.out.println(i + " " + iOb); // displays 100 100 
+    }
+}
