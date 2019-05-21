bc59eda17b405a79fa2a36f7873b5542120183cf
saltecrez <elisa.londero@inaf.it>
2019-05-21 20:33:53 +0200

    Static import and annotations

---
+++ src/chapter12/AnnoDemo.java
@@ -0,0 +1,10 @@
+package chapter12;
+
+class AnnoDemo {
+
+    public static void main(String args[]) {
+        MyClass myObj = new MyClass("test");
+
+        System.out.println(myObj.getMsg());
+    }
+}
---
+++ src/chapter12/MyClass.java
@@ -0,0 +1,22 @@
+package chapter12;
+
+// An example that uses @Deprecated. 
+
+// Deprecate a class. 
+@Deprecated
+class MyClass {
+
+    private String msg;
+
+    MyClass(String m) {
+        msg = m;
+    }
+
+    // Deprecate a method within a class. 
+    @Deprecated
+    String getMsg() {
+        return msg;
+    }
+
+    // ... 
+}
