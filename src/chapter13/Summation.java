de3fb82b4b93fdf995082b45b25074fa575468cd
saltecrez <elisa.londero@inaf.it>
2019-06-02 22:08:35 +0200

    Generic methods, constructors and interfaces

---
+++ src/chapter13/Summation.java
@@ -0,0 +1,19 @@
+package chapter13;
+
+// Use a generic constructor. 
+class Summation {
+
+    private int sum;
+
+    <T extends Number> Summation(T arg) {
+        sum = 0;
+
+        for (int i = 0; i <= arg.intValue(); i++) {
+            sum += i;
+        }
+    }
+
+    int getSum() {
+        return sum;
+    }
+}
